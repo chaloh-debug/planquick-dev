e35f9866ceeee0cf5803cda89721ea696ae2fb67
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-07-06 22:42:48 +0300

    whitenoise err fix

--- planquick/settings.py
+++ planquick/settings.py
@@ -140,7 +140,7 @@ STATICFILES_FINDERS = [
 "django.contrib.staticfiles.finders.FileSystemFinder",
 "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

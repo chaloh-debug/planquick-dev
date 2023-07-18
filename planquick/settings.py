b3eb0f79663a46d0ce893bccbe0d7a6c52a0dc71
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-07-18 17:32:48 +0300

    allowed host update

--- planquick/settings.py
+++ planquick/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://planquick-2c60bea645b4.herokuapp.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
